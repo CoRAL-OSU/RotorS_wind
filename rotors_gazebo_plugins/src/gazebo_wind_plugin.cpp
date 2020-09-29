82824a716568cf4bcec20a27b9cf85e7a64d0265
HB <he.bai@okstate.edu>
2020-09-28 22:57:22 -0500

    make the files to allow loading wind profile. however, it still crashes (memory issue?)

--- rotors_gazebo_plugins/src/gazebo_wind_plugin.cpp
+++ rotors_gazebo_plugins/src/gazebo_wind_plugin.cpp
@@ -23,7 +23,7 @@
 
 #include <fstream>
 #include <math.h>
-#include <string>
+#include <ros/ros.h>
 
 #include "ConnectGazeboToRosTopic.pb.h"
 
@@ -81,9 +81,7 @@ void GazeboWindPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
   // Check if a custom static wind field should be used.
   getSdfParam<bool>(_sdf, "useCustomStaticWindField", use_custom_static_wind_field_,
                       use_custom_static_wind_field_);
-  getSdfParam<ignition::math::Matrix3d>(_sdf, "dragCoefficient", drag_coefficient_,
-                      drag_coefficient_);
-
+  //ROS_INFO_STREAM("THIS IS MY FILE READING: " << use_custom_static_wind_field_);
   if (!use_custom_static_wind_field_) {
     gzdbg << "[gazebo_wind_plugin] Using user-defined constant wind field and gusts.\n";
     // Get the wind params from SDF.
@@ -102,19 +100,19 @@ void GazeboWindPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
     getSdfParam<ignition::math::Vector3d >(_sdf, "windGustDirection", wind_gust_direction_,
                         wind_gust_direction_);
 
-
     wind_direction_.Normalize();
     wind_gust_direction_.Normalize();
     wind_gust_start_ = common::Time(wind_gust_start);
     wind_gust_end_ = common::Time(wind_gust_start + wind_gust_duration);
+    ROS_INFO("NOT READING FILE\n");
   } else {
+    ROS_INFO("LOADING FILE\n");
     gzdbg << "[gazebo_wind_plugin] Using custom wind field from text file.\n";
     // Get the wind field text file path, read it and save data.
-
-    // HB: commented out as we will be doing this dynamically
-    getSdfParam<std::string>(_sdf, "customWindFieldPath", custom_wind_field_path_,
-                        custom_wind_field_path_);
-    //ReadCustomWindField(custom_wind_field_path);
+    std::string custom_wind_field_path;
+    getSdfParam<std::string>(_sdf, "customWindFieldPath", custom_wind_field_path,
+                        custom_wind_field_path);
+    ReadCustomWindField(custom_wind_field_path);
   }
 
   link_ = model_->GetLink(link_name_);
@@ -150,42 +148,27 @@ void GazeboWindPlugin::OnUpdate(const common::UpdateInfo& _info) {
     double wind_strength = wind_force_mean_;
     ignition::math::Vector3d wind = wind_strength * wind_direction_;
     // Apply a force from the constant wind to the link.
-    //link_->AddForceAtRelativePosition(wind, xyz_offset_);
-
-    // Apply drag force to link.
-
+    link_->AddForceAtRelativePosition(wind, xyz_offset_);
 
     ignition::math::Vector3d wind_gust(0.0, 0.0, 0.0);
-    ignition::math::Vector3d wind_rand(0.0, 0.0, 0.0);
     // Calculate the wind gust force.
     if (now >= wind_gust_start_ && now < wind_gust_end_) {
       double wind_gust_strength = wind_gust_force_mean_;
       wind_gust = wind_gust_strength * wind_gust_direction_;
-      double gn = ignition::math::Rand::DblNormal(0,wind_gust[0]);
-      double ge = ignition::math::Rand::DblNormal(0,wind_gust[1]);
-      double gd = ignition::math::Rand::DblNormal(0,wind_gust[2]);
-      wind_rand = (gn, ge, gd);
       // Apply a force from the wind gust to the link.
-      //link_->AddForceAtRelativePosition(wind_gust, xyz_offset_);
+      link_->AddForceAtRelativePosition(wind_gust, xyz_offset_);
     }
-
-    // added by HB
-    wind_velocity = wind + wind_rand;
-    ignition::math::Vector3d rel_wind= link_->WorldLinearVel() - wind_velocity;
-    ignition::math::Pose3d pose = link_->WorldPose();
-    ignition::math::Vector3d rel_body_vel = pose.Rot().RotateVector(rel_wind);
-    ignition::math::Vector3d drag_force = - rel_wind.Length() * drag_coefficient_ * rel_wind; 
-    // Apply drag force to link.
-    link_->AddRelativeForce(drag_force);
-
 
     wrench_stamped_msg_.mutable_header()->set_frame_id(frame_id_);
     wrench_stamped_msg_.mutable_header()->mutable_stamp()->set_sec(now.sec);
     wrench_stamped_msg_.mutable_header()->mutable_stamp()->set_nsec(now.nsec);
 
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_x(drag_force.X());
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_y(drag_force.Y());
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_z(drag_force.Z());
+    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_x(wind.X() +
+                                                                 wind_gust.X());
+    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_y(wind.Y() +
+                                                                 wind_gust.Y());
+    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_z(wind.Z() +
+                                                                 wind_gust.Z());
 
     // No torque due to wind, set x,y and z to 0.
     wrench_stamped_msg_.mutable_wrench()->mutable_torque()->set_x(0);
@@ -195,13 +178,8 @@ void GazeboWindPlugin::OnUpdate(const common::UpdateInfo& _info) {
     wind_force_pub_->Publish(wrench_stamped_msg_);
 
     // Calculate the wind speed.
-    //wind_velocity = wind_speed_mean_ * wind_direction_;
+    wind_velocity = wind_speed_mean_ * wind_direction_;
   } else {
-    // HB: here we can read in different files given simulation times
-    float simT = world_->SimTime().Float();
-    std::string custom_wind_field_path = std::to_string((int)simT) + custom_wind_field_path_;
-    ReadCustomWindField(custom_wind_field_path);
-
     // Get the current position of the aircraft in world coordinates.
     ignition::math::Vector3d link_position = link_->WorldPose().Pos();
 
@@ -310,30 +288,6 @@ void GazeboWindPlugin::OnUpdate(const common::UpdateInfo& _info) {
       // Set the wind velocity to the default constant value specified by user.
       wind_velocity = wind_speed_mean_ * wind_direction_;
     }
-
-    // He Bai TODO: Add drag force on the link based on the wind velocity
-    ignition::math::Vector3d rel_wind= link_->WorldLinearVel() - wind_velocity;
-    ignition::math::Pose3d pose = link_->WorldPose();
-    ignition::math::Vector3d rel_body_vel = pose.Rot().RotateVector(rel_wind);
-    ignition::math::Vector3d drag_force = - rel_wind.Length() * drag_coefficient_ * rel_wind; //0.1 is the DragCoefficient
-    // Apply drag force to link.
-    link_->AddRelativeForce(drag_force);
-
-    wrench_stamped_msg_.mutable_header()->set_frame_id(frame_id_);
-    wrench_stamped_msg_.mutable_header()->mutable_stamp()->set_sec(now.sec);
-    wrench_stamped_msg_.mutable_header()->mutable_stamp()->set_nsec(now.nsec);
-
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_x(drag_force.X());
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_y(drag_force.Y());
-    wrench_stamped_msg_.mutable_wrench()->mutable_force()->set_z(drag_force.Z());
-
-    // No torque due to wind, set x,y and z to 0.
-    wrench_stamped_msg_.mutable_wrench()->mutable_torque()->set_x(0);
-    wrench_stamped_msg_.mutable_wrench()->mutable_torque()->set_y(0);
-    wrench_stamped_msg_.mutable_wrench()->mutable_torque()->set_z(0);
-
-    wind_force_pub_->Publish(wrench_stamped_msg_);
-
   }
 
   wind_speed_msg_.mutable_header()->set_frame_id(frame_id_);
@@ -345,8 +299,6 @@ void GazeboWindPlugin::OnUpdate(const common::UpdateInfo& _info) {
   wind_speed_msg_.mutable_velocity()->set_z(wind_velocity.Z());
 
   wind_speed_pub_->Publish(wind_speed_msg_);
-
-
 }
 
 void GazeboWindPlugin::CreatePubsAndSubs() {
@@ -445,6 +397,7 @@ void GazeboWindPlugin::ReadCustomWindField(std::string& custom_wind_field_path) 
         // publish a message and ignore data on next line. Then resume reading.
         std::string restOfLine;
         getline(fin, restOfLine);
+        ROS_INFO("Invalid data name");
         gzerr << " [gazebo_wind_plugin] Invalid data name '" << data_name << restOfLine <<
               "' in custom wind field text file. Ignoring data on next line.\n";
         fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
